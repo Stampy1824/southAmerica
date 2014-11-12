f6e5440eaae0d2f9039283a6d890b91f1db894e4
Andrew Brock <andrewguntherbrock@gmail.com>
2014-11-12 17:44:30 -0600

    added cats, added rotation animation and bridge text

--- js/main.js
+++ js/main.js
@@ -20,7 +20,56 @@
         delay:75, 
         property:'left',
         start: 1600,
-        end: 640
+        end: 500
+    })           
+ });
+ //       $(document).ready(function() {
+ //        var scrollorama = $.scrollorama({
+ //            blocks:'.block'
+ //        });
+ //        scrollorama.animate('#trailstarttext3',{
+ //        duration:80, 
+ //        delay:250, 
+ //        property:'opacity',
+ //        start: 0,
+ //        end: 1
+
+ //    })           
+ // });
+       $(document).ready(function() {
+        var scrollorama = $.scrollorama({
+            blocks:'.block'
+        });
+        scrollorama.animate('#trailstarttext3',{
+        duration:800,
+        property:'rotate',
+        start: 720,
+
+    })           
+ });
+
+    $(document).ready(function() {
+        var scrollorama = $.scrollorama({
+            blocks:'.block'
+        });
+        scrollorama.animate('#bridgeviewtext1',{
+        duration:80, 
+        delay: 450,
+        property:'opacity',
+        start: 0,
+        end: 1
+    })           
+ });
+    $(document).ready(function() {
+        var scrollorama = $.scrollorama({
+            blocks:'.block'
+        });
+        scrollorama.animate('#bridgeviewtext2',{
+        duration:80, 
+        delay: 800,
+        property:'opacity',
+        start: 0,
+        end: 1
     })           
  });
 
